7b4a9fdd7b31b98d5cc8465289fec3391b140f02
JC Brand <jc@opkode.com>
2021-03-18 11:40:22 +0100

    Add rollup-plugin-includepaths

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,13 @@
+import includePaths from 'rollup-plugin-includepaths';
+
+const includePathOptions = {
+    include: {},
+    paths: ['node_modules'],
+    external: [],
+    extensions: ['.js']
+};
+
+
 export default {
   input: 'stashbounce.js',
   output: [
@@ -6,5 +16,6 @@ export default {
       format: 'iife',
       name: 'stashbounce',
     }
-  ]
+  ],
+  plugins: [ includePaths(includePathOptions) ],
 }
